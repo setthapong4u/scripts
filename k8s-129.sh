0a4a39b66d6939da1f0c155a2e34bd06b9abb386
setthapong4u <51050330+setthapong4u@users.noreply.github.com>
2024-09-23 13:14:41 +0700

    Update k8s-129.sh

--- k8s-129.sh
+++ k8s-129.sh
@@ -46,7 +46,10 @@ sudo apt-get install -y apt-transport-https curl
 
 # Add Kubernetes GPG key for version 1.29
 echo "Adding Kubernetes GPG key for version 1.29..."
-sudo mkdir -p /etc/apt/keyrings
+
+#curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 
 # Add Kubernetes repository for version 1.29
